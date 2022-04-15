3c0501b739b7dc159d7db2bfc4767434fa835763
PhOberd <phoberdorfer@edu.aau.at>
2022-04-15 17:23:59 +0200

    issue #5: Aligned UI Elements of Join Session Activity

--- app/src/main/java/at/aau/se2/gamma/carcassonne/views/JoinSessionActivity.java
+++ app/src/main/java/at/aau/se2/gamma/carcassonne/views/JoinSessionActivity.java
@@ -3,14 +3,22 @@ package at.aau.se2.gamma.carcassonne.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import at.aau.se2.gamma.carcassonne.R;
+import at.aau.se2.gamma.carcassonne.databinding.ActivityMainBinding;
 
 public class JoinSessionActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_join_session);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
+
+
 }
