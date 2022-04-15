2db426b0522d7a5873b2acb3b859e8d592517bc4
Martin Rader <mrader@MacBook-Pro-von-Martin.local>
2022-04-15 13:11:31 +0200

    issue-16-app: add ui elements screen

--- app/src/main/java/at/aau/se2/gamma/carcassonne/MainActivity.java
+++ app/src/main/java/at/aau/se2/gamma/carcassonne/MainActivity.java
@@ -1,9 +1,14 @@
 package at.aau.se2.gamma.carcassonne;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import at.aau.se2.gamma.carcassonne.views.UIElementsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn_ui_elements);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, UIElementsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
