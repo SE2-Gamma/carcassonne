72407aad569310f5a3aec3e5d7bbbaba28bd35ee
bthaus <bwuest@gmx.at>
2022-04-17 17:32:50 +0200

    ServerThread: added commandQueue

--- android/app/src/main/java/at/aau/se2/gamma/carcassonne/network/ServerThread.java
+++ android/app/src/main/java/at/aau/se2/gamma/carcassonne/network/ServerThread.java
@@ -2,14 +2,20 @@ package at.aau.se2.gamma.carcassonne.network;
 
 import android.util.Log;
 
+import com.badlogic.gdx.utils.compression.lzma.Base;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
 import java.util.Observable;
 
+import at.aau.se2.gamma.core.ServerResponse;
 import at.aau.se2.gamma.core.commands.BaseCommand;
 import at.aau.se2.gamma.core.commands.InitialSetNameCommand;
+import at.aau.se2.gamma.core.commands.ServerResponseCommand;
 
 public class ServerThread extends Thread {
 
@@ -26,6 +32,7 @@ public class ServerThread extends Thread {
     private Socket socket;
     public static ServerThread instance;
     ConnectionHandler connectionHandler;
+    private LinkedList<BaseCommand> commandQueue=new LinkedList<>();
 
     private ServerThread(String address, int port, ConnectionHandler connectionHandler) {
         this.address = address;
@@ -46,13 +53,25 @@ public class ServerThread extends Thread {
             objectInputStream = new ObjectInputStream(socket.getInputStream());
             connectionHandler.onConnectionFinished();
             while (true) {
-                sendCommand(new InitialSetNameCommand("mrader"));
-                BaseCommand command = (BaseCommand) objectInputStream.readObject();
-                connectionHandler.onServerResponse(command);
-                Log.d("RMLOG", "RESPONSE: "+command.getPayload());
+                BaseCommand command= null;
+
+                try {
+                    command = new InitialSetNameCommand("bodo");
+                    objectOutputStream.writeObject(command);
+                    BaseCommand response = (BaseCommand) objectInputStream.readObject(); //changed from command to response for clarity
+                    connectionHandler.onServerResponse(response);
+                    if(command.getRequestId().equals(response.getRequestId())) {
+                        Log.d("RMLOG", "RESPONSE: " + response.getPayload() + " Status:" + response.getState());
+                    }else{
+                        Log.e("RMLOG","Request ID doesnt match Response ID");
+                    }
+                } catch (NoSuchElementException e) { //todo: handle avoiding busy waiting
+                    e.printStackTrace();
+                }
                 sleep(1000*2);
             }
         } catch (Exception e) {
+
             connectionHandler.onServerFailure();
             Log.d("RMLOG", "HERE IN ERROR"+e.toString());
             e.printStackTrace();
@@ -60,15 +79,19 @@ public class ServerThread extends Thread {
     }
 
     public void sendCommand(BaseCommand command) {
+
+        commandQueue.add(command);
+
+
         // TODO: add request command to queue (because of the requestID), and
         // add a callback to this method, which is also in the queue
         // if we have a ServerResponse with the same requestID
         // return the response with the help of the callback
         // public void sendCommand(BaseCommand command, AnyCallbackInterface callback)
-        try {
+       /* try {
             objectOutputStream.writeObject(command);
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 }
