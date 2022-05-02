973c33a0cf9aa250715dd90dbff0a8ea4ee6f3dc
bthaus <bwuest@gmx.at>
2022-05-02 14:44:11 +0200

    issue-38: added diverse broadcasting messages

--- core/src/main/java/at/aau/se2/gamma/core/commands/PayloadBroadcastCommand.java
+++ core/src/main/java/at/aau/se2/gamma/core/commands/PayloadBroadcastCommand.java
@@ -1,2 +1,18 @@
-package at.aau.se2.gamma.core.commands;public class PayloadBroadcastCommand {
+package at.aau.se2.gamma.core.commands;
+
+import at.aau.se2.gamma.core.states.ClientState;
+
+public class PayloadBroadcastCommand extends BroadcastCommand {
+    public PayloadBroadcastCommand(Object payload) {
+        super(payload);
+    }
+    @Override
+    public String getKey() {
+        return "generic-paylaoad";
+    }
+
+    @Override
+    public ClientState getState() {
+        return ClientState.ANY;
+    }
 }
