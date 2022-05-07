126c6b8abb203a44f312521cc9ed3a120d9bd496
Martin Rader <m1rader@edu.aau.at>
2022-05-07 18:43:11 +0200

    issue-34: add GameMove

--- core/src/main/java/at/aau/se2/gamma/core/models/impl/PlayerListEntry.java
+++ core/src/main/java/at/aau/se2/gamma/core/models/impl/PlayerListEntry.java
@@ -1,2 +1,25 @@
-package at.aau.se2.gamma.core.models.impl;public class PlayerListEntry {
+package at.aau.se2.gamma.core.models.impl;
+
+import java.util.ArrayList;
+
+public class PlayerListEntry {
+    private Player player;
+    private int points = 0;
+    private ArrayList<Soldier> soldiers = new ArrayList<>();
+
+    public PlayerListEntry(Player player) {
+        this.player = player;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public ArrayList<Soldier> getSoldiers() {
+        return soldiers;
+    }
 }
