19f5f937d3b708a79df0a88d557be6c2f0697e33
Markus Ulbing <m1ulbing@edu.aau.at>
2022-05-08 21:36:15 +0200

    issue-4: bugfix

--- android/app/src/main/java/at/aau/se2/gamma/carcassonne/views/CreateSessionActivity.java
+++ android/app/src/main/java/at/aau/se2/gamma/carcassonne/views/CreateSessionActivity.java
@@ -1,13 +1,12 @@
 package at.aau.se2.gamma.carcassonne.views;
 
-import at.aau.se2.gamma.carcassonne.base.BaseActivity;
-
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import at.aau.se2.gamma.carcassonne.base.BaseActivity;
 import at.aau.se2.gamma.carcassonne.databinding.ActivityCreateSessionBinding;
 import at.aau.se2.gamma.carcassonne.network.ServerThread;
 import at.aau.se2.gamma.carcassonne.views.lobby.LobbyActivity;
@@ -18,6 +17,7 @@ import at.aau.se2.gamma.core.commands.CreateGameCommand;
 public class CreateSessionActivity extends BaseActivity {
 
     public ActivityCreateSessionBinding binding;
+    String sessionName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +25,6 @@ public class CreateSessionActivity extends BaseActivity {
         binding = ActivityCreateSessionBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-
-        String sessionName = "";
 
         binding.buttonNavigateLobby.setVisibility(View.INVISIBLE);
         binding.textViewError.setVisibility(View.INVISIBLE);
@@ -36,9 +34,8 @@ public class CreateSessionActivity extends BaseActivity {
             @Override
             public void onClick(View view) {
                 binding.progressBarJoinSessionActivity.setVisibility(View.VISIBLE);
-                String sessionName = binding.editTextSessionname.getText().toString();
+                sessionName = binding.editTextSessionname.getText().toString();
                 if(sessionName.length()>0) {
-                    Log.d("SOUT", "if");
                     CreateSessionActivity.this.sendServerCommand(new CreateGameCommand(sessionName), new ServerThread.RequestResponseHandler() {
                         @Override
                         public void onResponse(ServerResponse response, Object payload, BaseCommand request) {
@@ -49,7 +46,6 @@ public class CreateSessionActivity extends BaseActivity {
                             binding.progressBarJoinSessionActivity.setVisibility(View.INVISIBLE);
                             binding.buttonCreateSession.setVisibility(View.INVISIBLE);
                         }
-
                         @Override
                         public void onFailure(ServerResponse response, Object payload, BaseCommand request) {
                             binding.textViewError.setVisibility(View.VISIBLE);
