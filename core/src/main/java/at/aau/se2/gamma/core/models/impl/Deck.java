f446090adb15afe400658c873bc2b5d419892543
bthaus <bwuest@gmx.at>
2022-05-09 13:08:04 +0200

    issue-38: Created Deckclass for real

--- core/src/main/java/at/aau/se2/gamma/core/models/impl/Deck.java
+++ core/src/main/java/at/aau/se2/gamma/core/models/impl/Deck.java
@@ -1,2 +1,14 @@
-package at.aau.se2.gamma.core.models.impl;public class Deck {
+package at.aau.se2.gamma.core.models.impl;
+
+import at.aau.se2.gamma.core.factories.GameCardFactory;
+
+import java.util.LinkedList;
+
+public class Deck {
+    LinkedList<GameCard>deck=new LinkedList<>();
+
+    public Deck(int multfaktor) {
+        deck= GameCardFactory.getDeck(multfaktor);
+    }
+
 }
