f0587fc2e1aa78e364d047f049192bd178bbee5d
PhOberd <phoberdorfer@edu.aau.at>
2022-05-09 17:10:57 +0200

    addre

--- core/src/main/java/at/aau/se2/gamma/core/commands/LeaveLobbyCommand.java
+++ core/src/main/java/at/aau/se2/gamma/core/commands/LeaveLobbyCommand.java
@@ -1,2 +1,14 @@
-package at.aau.se2.gamma.core.commands;public class LeaveLobbyCommand {
+package at.aau.se2.gamma.core.commands;
+
+import at.aau.se2.gamma.core.states.ClientState;
+
+public class LeaveLobbyCommand extends BaseCommand{
+
+    public LeaveLobbyCommand(Object payload){super(payload);}
+
+    @Override
+    public String getKey() { return "leaveLobby"; }
+
+    @Override
+    public ClientState getState() { return ClientState.INITIAl; }
 }
