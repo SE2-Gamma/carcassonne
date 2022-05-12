ac128802d741fda985a2971cacb84bee8deed2ca
Markus Ulbing <m1ulbing@edu.aau.at>
2022-05-12 16:52:51 +0200

    issue-4: add simple tests

--- android/app/src/main/java/at/aau/se2/gamma/carcassonne/views/CreateSessionActivity.java
+++ android/app/src/main/java/at/aau/se2/gamma/carcassonne/views/CreateSessionActivity.java
@@ -30,44 +30,46 @@ public class CreateSessionActivity extends BaseActivity {
         binding.textViewError.setVisibility(View.INVISIBLE);
         binding.progressBarJoinSessionActivity.setVisibility(View.INVISIBLE);
 
-        binding.buttonCreateSession.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                binding.progressBarJoinSessionActivity.setVisibility(View.VISIBLE);
-                sessionName = binding.editTextSessionname.getText().toString();
-                if(sessionName.length()>0) {
-                    CreateSessionActivity.this.sendServerCommand(new CreateGameCommand(sessionName), new ServerThread.RequestResponseHandler() {
-                        @Override
-                        public void onResponse(ServerResponse response, Object payload, BaseCommand request) {
-                            binding.textViewError.setVisibility(View.VISIBLE);
-                            binding.textViewError.setText("Session created!");
-                            binding.textViewError.setTextColor(Color.BLACK);
-                            binding.buttonNavigateLobby.setVisibility(View.VISIBLE);
-                            binding.progressBarJoinSessionActivity.setVisibility(View.INVISIBLE);
-                            binding.buttonCreateSession.setVisibility(View.INVISIBLE);
-                        }
-                        @Override
-                        public void onFailure(ServerResponse response, Object payload, BaseCommand request) {
-                            binding.textViewError.setVisibility(View.VISIBLE);
-                            binding.textViewError.setText("Error");
-                            binding.progressBarJoinSessionActivity.setVisibility(View.INVISIBLE);
-                        }
-                    });
-                }else {
-                    binding.progressBarJoinSessionActivity.setVisibility(View.INVISIBLE);
-                    binding.textViewError.setVisibility(View.VISIBLE);
-                    binding.textViewError.setText("Choose a name for your game!");
-                }
-            }
-        });
+        binding.buttonCreateSession.setOnClickListener(this::createSession);
+        binding.buttonNavigateLobby.setOnClickListener(this::navigateToLobby);
+    }
 
-        binding.buttonNavigateLobby.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(CreateSessionActivity.this, LobbyActivity.class);
-                intent.putExtra("GameKey", sessionName);
-                startActivity(intent);
-            }
-        });
+    public void createSession(View view) {
+        binding.progressBarJoinSessionActivity.setVisibility(View.VISIBLE);
+        sessionName = binding.editTextSessionname.getText().toString();
+        if(sessionName.length()>0) {
+            CreateSessionActivity.this.sendServerCommand(new CreateGameCommand(sessionName), new ServerThread.RequestResponseHandler() {
+                @Override
+                public void onResponse(ServerResponse response, Object payload, BaseCommand request) {
+                    binding.textViewError.setVisibility(View.VISIBLE);
+                    binding.textViewError.setText("Session created!");
+                    binding.textViewError.setTextColor(Color.BLACK);
+                    binding.buttonNavigateLobby.setVisibility(View.VISIBLE);
+                    binding.progressBarJoinSessionActivity.setVisibility(View.INVISIBLE);
+                    binding.buttonCreateSession.setVisibility(View.INVISIBLE);
+                }
+                @Override
+                public void onFailure(ServerResponse response, Object payload, BaseCommand request) {
+                    binding.textViewError.setVisibility(View.VISIBLE);
+                    binding.textViewError.setText("Error");
+                    binding.progressBarJoinSessionActivity.setVisibility(View.INVISIBLE);
+                }
+            });
+        }else {
+            binding.progressBarJoinSessionActivity.setVisibility(View.INVISIBLE);
+            binding.textViewError.setVisibility(View.VISIBLE);
+            binding.textViewError.setText("Choose a name for your game!");
+        }
+    }
+
+    public void navigateToLobby(View view) {
+        Intent intent = new Intent(CreateSessionActivity.this, LobbyActivity.class);
+        intent.putExtra("GameKey", sessionName);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
