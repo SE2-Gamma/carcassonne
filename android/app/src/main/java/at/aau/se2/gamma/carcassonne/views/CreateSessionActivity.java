2fa47804e1bb81047283d1aee52313e15651ff0a
Markus Ulbing <m1ulbing@edu.aau.at>
2022-05-29 18:27:29 +0200

    issue-4: refactoring

--- android/app/src/main/java/at/aau/se2/gamma/carcassonne/views/CreateSessionActivity.java
+++ android/app/src/main/java/at/aau/se2/gamma/carcassonne/views/CreateSessionActivity.java
@@ -1,13 +1,15 @@
 package at.aau.se2.gamma.carcassonne.views;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.LinkedList;
 
 import at.aau.se2.gamma.carcassonne.base.BaseActivity;
 import at.aau.se2.gamma.carcassonne.databinding.ActivityCreateSessionBinding;
 import at.aau.se2.gamma.carcassonne.network.ServerThread;
+import at.aau.se2.gamma.carcassonne.utils.Logger;
 import at.aau.se2.gamma.carcassonne.views.lobby.LobbyActivity;
 import at.aau.se2.gamma.core.ServerResponse;
 import at.aau.se2.gamma.core.commands.BaseCommand;
@@ -37,42 +39,41 @@ public class CreateSessionActivity extends BaseActivity {
     public void createSession(View view) {
         binding.progressBarJoinSessionActivity.setVisibility(View.VISIBLE);
         sessionName = binding.editTextSessionname.getText().toString();
+
         if(sessionName.length()>0) {
             CreateSessionActivity.this.sendServerCommand(new CreateGameCommand(sessionName), new ServerThread.RequestResponseHandler() {
+
                 @Override
                 public void onResponse(ServerResponse response, Object payload, BaseCommand request) {
-                    /*
-                    binding.textViewError.setVisibility(View.VISIBLE);
-                    binding.textViewError.setText("Session created!");
-                    binding.textViewError.setTextColor(Color.BLACK);
-                    */
+                    Logger.debug("onResponsePayload: " + payload.toString());
+
                     binding.progressBarJoinSessionActivity.setVisibility(View.INVISIBLE);
                     binding.buttonCreateSession.setVisibility(View.INVISIBLE);
 
-                    navigateToLobby(view);
+                    Intent intent = new Intent(CreateSessionActivity.this, LobbyActivity.class);
+                    Bundle extras = new Bundle();
+                    extras.putString("GameKey", sessionName);
+                    extras.putString("UserName", userName);
+                    intent.putExtras(extras);
+                    startActivity(intent);
                 }
+
                 @Override
                 public void onFailure(ServerResponse response, Object payload, BaseCommand request) {
+                    Logger.debug("onFailurePayload: " + payload.toString());
+
+                    binding.textViewError.setText(((LinkedList<String>)payload).get(0));
                     binding.textViewError.setVisibility(View.VISIBLE);
-                    binding.textViewError.setText("Servererror");
                     binding.progressBarJoinSessionActivity.setVisibility(View.INVISIBLE);
                 }
+
             });
           
         }else {
-            binding.progressBarJoinSessionActivity.setVisibility(View.INVISIBLE);
-            binding.textViewError.setVisibility(View.VISIBLE);
             binding.textViewError.setText("Choose a name for your game!");
+            binding.textViewError.setVisibility(View.VISIBLE);
+            binding.progressBarJoinSessionActivity.setVisibility(View.INVISIBLE);
         }
-    }
-
-    public void navigateToLobby(View view) {
-        Intent intent = new Intent(CreateSessionActivity.this, LobbyActivity.class);
-        Bundle extras = new Bundle();
-        extras.putString("GameKey", sessionName);
-        extras.putString("UserName", userName);
-        intent.putExtras(extras);
-        startActivity(intent);
     }
 
     @Override
